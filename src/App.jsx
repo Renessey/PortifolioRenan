7ab070afffb16b4d713a06fb285a3c3f68ca9729
RENAN <renan@MacBook-Pro-de-RENAN.local>
2026-03-10 18:05:16 -0300

    Att 1.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar"
-import Tela1 from "./components/tela1"
-import Tela2 from "./components/tela2"
+import Tela1 from "./components/Tela1"
+import Tela2 from "./components/Tela2"
 import Tela3 from "./components/Tela3"
 import Tela4 from "./components/Tela4"
 import "./App.css"
